3f365c203110fec930900cbae0dbfffedf4418dc
moonbot <moonbot@moonbot.local>
2016-12-19 11:40:31 -0500

    Initial commit

---
+++ lifeshooter/sketch.js
@@ -0,0 +1,14 @@
+var stars = [],
+    WIDTH = window.innerWidth,
+    HEIGHT = window.innerHeight,
+    FPS = 60, // Frames per second
+    //NUM_STARS = WIDTH; // Number of stars
+    NUM_STARS = 10;
+
+function setup() {
+  
+}
+
+function draw() {
+  
+}
