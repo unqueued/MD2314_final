0e0cb49ca4be052480328bcd0910aa99e121737e
moonbot <moonbot@moonbot.local>
2016-12-19 12:37:01 -0500

    Got star scrolling working. Gonna refactor it with classes, now

--- lifeshooter/sketch.js
+++ lifeshooter/sketch.js
@@ -13,24 +13,62 @@ var
 
 function setup() {
   createCanvas(WIDTH, HEIGHT);
+  
+  // Initialize onscreen entities
+  for(var i = 0; i < NUM_OF_STARS; i++) {
+    stars.push({
+      x: Math.random() * WIDTH,
+      y: Math.random() * HEIGHT
+    });
+  }
+  
   frameRate(FPS);
   loop();
 }
 
 function draw() {
+  
   background(0, 0, 0);
   
-  
+  // Draw stars
+  // There is much debate about best practices for iteration in js...
+  for(var i = 0; i < NUM_OF_STARS; i++) {
+    fill(255, 255, 255);
+    noStroke();
+    ellipse(stars[i].x, stars[i].y, 10, 10);
+  }
   
   update();
+}
+
+
+
+function update() {
+  //console.log("Updating...");
+  //console.log("Star: " + stars[2].x);
+  for(var i = 0; i < NUM_OF_STARS; i++) {
+    stars[i].y++;
+    if(stars[i].y > HEIGHT) {
+      stars[i].y = 0;
+      stars[i].x = Math.random() * WIDTH;
+    }
+  }
+}
+
+function mouseMoved() {
+  return false;
+}
+
+function mouseDragged() {
+
+}
+
+function mousePressed() {
+
 }
 
 function windowResized() {
     WIDTH = window.innerWidth,
     HEIGHT = window.innerHeight,
     resizeCanvas(WIDTH, HEIGHT);
-}
-
-function update() {
-  
 }
