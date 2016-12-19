f83800a689f635ca714d6e3476f3397d03ffb778
moonbot <moonbot@moonbot.local>
2016-12-19 12:02:52 -0500

    For some reason screen is not updating properly and update() is not working

--- lifeshooter/sketch.js
+++ lifeshooter/sketch.js
@@ -1,14 +1,32 @@
-var stars = [],
+var
+    //WIDTH = 600,
+    //HEIGHT = 600,
     WIDTH = window.innerWidth,
     HEIGHT = window.innerHeight,
-    FPS = 60, // Frames per second
-    //NUM_STARS = WIDTH; // Number of stars
-    NUM_STARS = 10;
+    FPS = 60; // Frames per second
+    hearts = [],
+    NUM_OF_HEARTS = 3,
+    
+    stars = [],
+    NUM_OF_STARS = 20,
+    score = 0;
+    
 
 function setup() {
-  
+  createCanvas(800, 600);
+  console.log("Created canvas");
+  //createCanvas(WIDTH, HEIGHT);
+  frameRate(FPS);
+  loop();
 }
 
 function draw() {
-  
+  background(0, 0, 0);
+  //console.log(frameCount);
+}
+
+function windowResized() {
+    WIDTH = window.innerWidth,
+    HEIGHT = window.innerHeight,
+    resizeCanvas(WIDTH, HEIGHT);
 }
