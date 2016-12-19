fb9e1b20b56d6a33aa6b40f533eddb0cfad51952
moonbot <moonbot@moonbot.local>
2016-12-19 16:44:02 -0500

    It will have to be good enough, finishing touches

--- lifeshooter/sketch.js
+++ lifeshooter/sketch.js
@@ -19,7 +19,12 @@ var
     tmpHeartImage = null,
     tmpBrokenHeartImage = null,
     missedHearts = 0,
-    endingState = false;
+    endingState = false,
+    finalHeart = null,
+    missedHeartsThreshold = 10,
+    brokenHeartsThreshold = 10,
+    scoreHeart = null,
+    creditsState = false;
     
 function preload() {
   //tmpHeartImage = loadImage("lifeshooter/images/Heart.svg");
@@ -78,6 +83,10 @@ function draw() {
     hearts[i].display();
   }
   
+  if(endingState) {
+    finalHeart.display();
+  }
+  
   //image(tmpHeartImage, 50, 50, 80, 80);
   
   drawScore();
@@ -94,6 +103,10 @@ function update() {
   for(var i = 0; i < NUM_OF_HEARTS; i++) {
     hearts[i].move();
   }
+  
+  if(endingState) {
+    finalHeart.move();
+  }
 }
 
 function drawScore() {
@@ -102,18 +115,37 @@ function drawScore() {
   textSize(32);
   //text("High score: " + score, 20, 30);
   text("Score:", 20, 30);
-  image(tmpHeartImage, 38, 55, 40, 40);
-  text(" X " + missedHearts, 55, 65);
+  
+  if(!creditsState) {
+    image(tmpHeartImage, 38, 55, 40, 40);
+    text(" X " + score, 55, 65);
+  } else {
+    if(scoreHeart != null) {
+      scoreHeart.move();
+      scoreHeart.display();
+      text("Congratulations", (WIDTH / 2) - 100, HEIGHT / 3);
+    }
+    //image(tmpHeartImage, 38, 55, 40, 40);
+    text(" X " + score, 55, 65);
+  }
   image(tmpBrokenHeartImage, 38, 95, 35, 35);
   text(" X " + brokenHeartScore, 55, 105);
 }
 
 function startEndingState() {
+  // As the other hearts fade, this new one starts to rise
   endingState = true;
   for(var i = 0; i < NUM_OF_HEARTS; i++) {
     hearts[i].fading = true;
     hearts[i].alive = false;
   }
+  finalHeart = new Heart();
+  finalHeart.x = WIDTH / 2;
+  finalHeart.y = HEIGHT;
+  finalHeart.speed = -1;
+  //finalHeart.size = finalHeart.size * 2;
+  finalHeart.isFinalHeart = true;
+  //hearts.push(finalHeart); // So that collision detection detects it
 }
 
 function mouseMoved() {
@@ -131,6 +163,21 @@ function keyPressed() {
 }
 
 function mousePressed() {
+  
+  if(endingState) {
+    if(mouseX > finalHeart.x-15 && mouseX < finalHeart.x+15) {
+      finalHeart.clickedOn();
+      //finalHeart.x = 38;
+      //finalHeart.y = 55;
+      score++;
+      console.log("GAME IS ENDED");
+      creditsState = true;
+      scoreHeart = new Heart();
+      scoreHeart.x = 38;
+      scoreHeart.y = 55;
+      scoreHeart.speed = 0;
+    }
+  }
   
   //console.log("Testing X: " + mouseX + " Y: " + mouseY);
   // This really belongs inside the class... but since I'm jsut gonna only have one clickable thing, then w/e
@@ -181,7 +228,7 @@ function Star() {
 function Heart() {
   // This is really messy and I'm sure there is a much more elllegant way to do it...
   this.x = Math.random() * WIDTH;
-  this.y = Math.random() * -3000;
+  this.y = Math.random() * -1000;
   this.speed = 1;
   this.diameter = 4;
   this.size = 30 - (Math.random() * 5);
@@ -192,6 +239,7 @@ function Heart() {
   this.fading = false;
   this.myTint = 255;
   this.layer = 0;
+  this.isFinalHeart = false;
   
   minBeatSize = 7;
   this.currentSize = this.size;
@@ -200,7 +248,13 @@ function Heart() {
   //console.log("Initialized heart with size: " + this.size);
   
   this.move = function() {
-    this.y += this.speed;
+    if(this.isFinalHeart) {
+      if(this.y > HEIGHT / 2) {
+        this.y += this.speed;
+      }
+    } else {
+      this.y += this.speed;
+    }
     
     if(this.fading) {
       return;
@@ -216,8 +270,9 @@ function Heart() {
     
     if(this.y > HEIGHT) {
       missedHearts++;
+      console.log("MissedHearts: " + missedHearts)
       this.alive = false;
-      if(missedHearts > 8) {
+      if(missedHearts > missedHeartsThreshold) {
         startEndingState();
       }
       //this.y = 0;
@@ -276,6 +331,17 @@ function Heart() {
   }
   
   this.clickedOn = function() {
+    if(this.isFinalHeart) {
+      if(this.y >= HEIGHT / 2) {
+        return;
+      } else {
+        console.log("IM DOING THIS BECAUSE " + this.y + " >= " + HEIGHT / 2);
+        //score++;
+        this.fading = true;
+        this.alive = false;
+      }
+    }
+    
     if(!this.alive) {
       return;
     }
@@ -294,7 +360,7 @@ function Heart() {
       this.currentSize = this.size;
       this.fading = true;
       brokenHeartScore++;
-      if(brokenHeartScore > 8) {
+      if(brokenHeartScore > missedHeartsThreshold) {
         startEndingState();
       }
     }
